de63538af65fe9dd67aa4d2f4b4832cd116b75e0
Rom <romain.droy.pro@gmail.com>
2021-11-04 13:48:20 +0100

    !! CHANGED userModel // Added ItemModel (WorkInProgress) //

---
+++ models/itemModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const itemSchema = mongoose.Schema({
+    name: String, 
+    itemInfos: {
+        adress:String,
+        city:String,
+        type:String,
+        livingArea:String,
+        rooms:String,
+        bedrooms:String,
+        terraceSurface:Number,
+        garage:Number,
+        parking:Number,
+        plotArea:Number,
+        
+    },
+    funding:{
+        initialStableData:
+        priceInEuros:Number,
+        initialTokenAmount:Number,
+        initialTokenValueInEuros:Number,
+        
+        remainingAvailableToken:Number,
+
+        fundingStartDate:String, 
+        fundingEndDeadlineDate:String,
+        fundingGoalReachedDate:String, 
+
+        
+
+    }
+
+})
--- models/userModel.js
+++ models/userModel.js
@@ -3,19 +3,24 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   firstName: String,
   lastName: String,
+  userName:String,
   email: String,
   password: String,
-  info: {
-    telephone: String,
-    stableCoins: Number,
-    adresse: String,
-    ville: String,
-    codePostal: String,
-    dateOfBirth: String,
-    finaliser: Boolean,
-    admin: Boolean,
+  stableCoins: Number,
+
+  infos: {
+    phoneNumber: String,
+    nationality:String,
+    dateOfBirth:String,
+    streetNumber:String,
+    streetName:String,
+    city:String, 
+    postalCode:String,
+    country:String,
+    isAdmin:Boolean,
+    isVerified:Boolean,
   },
-  biens: [{ idBien: String, quantitieBien: Number, dateAchat: String }],
+  ownedItems: [{ itemId: String, tokenQuantity: Number, purchaseDate: String }],
 });
 
 const UserModel = mongoose.model("users", userSchema);
