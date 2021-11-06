f5cdbfafc1d2727df9fcc8d4a0649a89be6e5fd7
pal-14 <pal14@live.fr>
2021-11-06 19:46:09 +0100

    ///////////////////photo ds back///////////

--- app.js
+++ app.js
@@ -3,14 +3,72 @@ const cors = require('cors')
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+const multer = require('multer')////////////
+const ejs = require('ejs')/////////////
+const path = require ('path')//////////////
+
+
+
+
+
+
+
+
+
 require('dotenv').config();
 require('./connectDb')
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 
+//////////////////////////////////set storage
+const storage = multer.diskStorage({
+    destination: './public/uploads',
+    filename :function  (req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+
+/////init upload
+
+const upload = multer({
+    storage: storage,
+}).array('myImage');
+
+
+
+
 let app = express();
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///ejs
+app.set('view engine', 'ejs')//////////////
+///public folder
+app.use(express.static('/public'))//////////////
+
+
+
+app.get('/',(req , res ) =>res.render('index'))//////////
+
+app.post('/upload', (req, res)=>{
+    upload(req, res, (err)=> {
+        if (err){
+            res.render('index', {
+                msg : err
+            });
+        } else {
+            console.log(req.file);
+            res.send(`Vos fichiers sont sont en traitement dans notre base de donnés.
+            Vous pouvez continuer de visiter notre site en attendant
+             qu'un administrateur valide votre compte`);
+        }
+    }) 
+})
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.use(cors());
 app.use(logger('dev'));
 
